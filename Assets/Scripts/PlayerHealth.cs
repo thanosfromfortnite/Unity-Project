645755e34a1260183587db5f1385d0a3cbdbd546
Jesse <jhan3537@bths.edu>
2019-03-01 11:37:10 -0500

    serfsdsdfsdfsdf

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float MaxHealth;
+    [SerializeField] private bool StartWithMaxHealth;
+    [SerializeField] public float Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (StartWithMaxHealth)
+        {
+            Health = MaxHealth;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
