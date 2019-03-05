9041c32a89ac769d15db88285996c496c741dc59
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2019-03-05 11:05:56 -0500

    awd

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,7 +9,7 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int Health;
     private int damage = 0;
     public Animator animator;
-    private int x = 0;
+    private bool x = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,21 +23,30 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+       
         Health -= damage;
         damage = 0;
         // If an outside source sets or adds damage, take that damage immediately and wait for another update to it
-        
+        if (Input.GetButtonDown("Jump") && !x)
+        {
+            x = true;
+            Debug.Log("no");
+            Ouch(5);
+        }
+
+
     }
-    public void Ouch(int dmg)
+    public IEnumerator Ouch(int dmg)
     {
         // Flinch effect
         damage += dmg;
-        GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0.8f);
-        if (x > 5)
-        {
-            x = 0;
-            GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0);
-            Debug.Log("yeah");
-        }
+        Debug.Log("yeah");
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+        yield return new WaitForSeconds(1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Debug.Log("yeah");
+        x = false;
     }
 }
