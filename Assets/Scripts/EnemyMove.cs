150973382091e81f25f3c778abd12806dff98734
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2019-03-20 11:15:37 -0400

    dfgdgr

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Rigidbody2D m_Rigidbody2D;
+    [SerializeField] private float JumpForce = 7f;
+    [SerializeField] private float HorizontalJumpForce = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveYouIdiot(bool right)
+    {
+        if (right)
+            Move(HorizontalJumpForce, 0f);
+        else
+            Move(-HorizontalJumpForce, 0f);
+    }
+
+    public void JumpYouIdiot(bool right)
+    {
+        if (right)
+        {
+            Move(HorizontalJumpForce, JumpForce);
+        }
+        else
+        {
+            Move(-HorizontalJumpForce, JumpForce);
+        }
+    }
+
+    private void Move(float xForce, float yForce)
+    {
+        m_Rigidbody2D.velocity = new Vector3(xForce, yForce);
+        m_Rigidbody2D.AddForce(new Vector2(xForce, yForce));
+    }
+}
---
+++ Assets/Scripts/PlayerDetectionForThingsThatArentFrogs.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetectionForThingsThatArentFrogs : MonoBehaviour
+{
+    [SerializeField] private float DetectionRadius = 5f;
+    [SerializeField] private float CooldownTimer = 3f;
+    [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private Transform WallCheckLeft;
+    [SerializeField] private Transform WallCheckRight;
+    
+    const float k_GroundedRadius = .5f;
+    private Transform ownPosition;
+    private Transform playerPosition;
+    public EnemyMove enemyMovement;
+    private float relativePosition;
+    [SerializeField] private bool DontMoveIfNotGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.Find("Player").transform;
+        ownPosition = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Detect();
+    }
+
+    private void Detect()
+    {
+        bool obstacle = false;
+        bool m_Grounded = false;
+        
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+            }
+        }
+        if ((m_Grounded && DontMoveIfNotGrounded || !DontMoveIfNotGrounded) && Mathf.Abs(Mathf.Abs(playerPosition.position.x + playerPosition.position.y) - Mathf.Abs(ownPosition.position.x + ownPosition.position.y)) <= DetectionRadius)
+        {
+            Collider2D[] otherColliders = Physics2D.OverlapCircleAll(WallCheckLeft.position, k_GroundedRadius, m_WhatIsGround);
+            for (int i = 0; i < otherColliders.Length; i ++)
+            {
+                if (otherColliders[i].gameObject != gameObject)
+                {
+                    obstacle = true;
+                }
+            }
+            Collider2D[] rightColliders = Physics2D.OverlapCircleAll(WallCheckRight.position, k_GroundedRadius, m_WhatIsGround);
+            for (int i = 0; i < rightColliders.Length; i ++)
+            {
+                if (rightColliders[i].gameObject != gameObject)
+                {
+                    obstacle = true;
+                }
+            }
+            if (obstacle && m_Grounded)
+            {
+                enemyMovement.JumpYouIdiot(playerPosition.position.x - ownPosition.position.x >= 0);
+            }
+            else
+            {
+                enemyMovement.MoveYouIdiot(playerPosition.position.x - ownPosition.position.x >= 0);
+            }
+        }
+    }
+}
