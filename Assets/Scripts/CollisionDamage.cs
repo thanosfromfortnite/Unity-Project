f014ac0cd214cc126fa9f0579e3fe3289f27c37a
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2019-06-12 11:45:25 -0400

    awdawdawd

--- Assets/Scripts/CollisionDamage.cs
+++ Assets/Scripts/CollisionDamage.cs
@@ -9,6 +9,7 @@ public class CollisionDamage : MonoBehaviour
     public CircleCollider2D objectCollider;
     [SerializeField] public LayerMask PlayerLayer;
     [SerializeField] public float knockbackX, knockbackY;
+    [SerializeField] public int Damage = 1;
 
     
     void Start()
@@ -19,12 +20,12 @@ public class CollisionDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(objectCollider.transform.position, objectCollider.radius, PlayerLayer);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(objectCollider.transform.position, objectCollider.radius * 5, PlayerLayer);
         for (int i = 0; i < colliders.Length; i ++)
         {
             if (colliders[i].gameObject.tag == "Player")
             {
-                colliders[i].gameObject.GetComponent<PlayerHealth>().TakeDamage((gameObject.transform.position.x - colliders[i].gameObject.transform.position.x) > 0);
+                colliders[i].gameObject.GetComponent<PlayerHealth>().TakeDamage((gameObject.transform.position.x - colliders[i].gameObject.transform.position.x) > 0, Damage, knockbackX, knockbackY);
                 break;
             }
         }
